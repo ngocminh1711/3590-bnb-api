2aecad8198ca77400b41f90570d0b68ae6fa935e
ngo minh ngoc <ngoc.ngominh1711@gmail.com>
2022-10-31 15:27:08 +0700

    add file

--- src/models/DBconnect.js
+++ src/models/DBconnect.js
@@ -4,7 +4,7 @@ import mongoose  from "mongoose";
 
 class DBconnect {
     async connect() {
-        await mongoose.connect('mongodb+srv://thao:thao1234@casestudy4.hrswjtf.mongodb.net/Casestudy4')
+        await mongoose.connect('mongodb+srv://thao:thao1234@casestudy4.hrswjtf.mongodb.net/3590bnb')
     }
 }
 
