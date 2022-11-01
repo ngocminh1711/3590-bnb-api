74998c6619c71e24a1e5d7725965b67d518f0d6a
Pham Thanh Thao <37079867+thaoss2ss@users.noreply.github.com>
2022-11-01 14:40:20 +0700

    thao

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from "cors"
 import  DBconnect  from "./src/models/DBconnect.js";
 
+import bodyParser from "express";
+
 
 
 const app = express();
@@ -10,12 +12,17 @@ app.use(cors());
 const PORT = process.env.PORT || 8000;
 
 const db = new DBconnect()
+app.use(bodyParser.json());
+
+
 
 db.connect().then( () => {
     console.log('DB connected')
 }).catch(err => {
     console.log(err.message)
 })
+
+
 
 
 
